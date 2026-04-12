04836c4aead2285e6b78efb8f499a0509a247950
tasbeeh479 <tasbeehullah2007@gmail.com>
2026-04-12 10:07:23 +0500

    Learning syntax

---
+++ 01_BASICS/01_Start.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to C++" << endl;
+    cout << "Let's start coding!" << endl;
+    
+    return 0;
+}
+
+
+/* #include <iostream>: This line includes the iostream library, which allows 
+us to use input and output functions such as cout for printing to the console. */
+
+/* using namespace std;: This line allows us to use names from the standard
+namespace without having to prefix them with std:: (like std::cout). It simplifies our code when 
+using standard library features. */
+
+/* int main(): This is the main function where the execution of the program begins.
+The return type is int, which means the function will return an integer value. The parameters are empty, 
+indicating that the function does not take any input arguments. While Braces {} enclose the body of the function. */
+
+/* cout << "Welcome to C++" << endl;: This line uses the cout object to print the string "Welcome to C++"
+to the console, followed by endl, which inserts a newline character and flushes the output buffer. */
+
+/* cout << "Let's start coding!" << endl;: This line prints the string "Let's start coding!" to the console,
+followed by a newline. */
+
+/* return 0;: This line indicates that the program has finished successfully.
+In C++, the main function must return an integer value. Returning 0 typically indicates that the program executed successfully without any errors. */
