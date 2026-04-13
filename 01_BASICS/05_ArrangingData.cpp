2f523cefeb5b0667cfabb46c30d5b316c1eb6846
tasbeeh479 <tasbeehullah2007@gmail.com>
2026-04-13 21:59:37 +0500

    Data Arranging

---
+++ 01_BASICS/05_ArrangingData.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Item Name\tQuantity\tPrice" << endl;
+    cout <<"      Pen"<<"\t      10"<<"        15.5" << endl;
+    cout <<"Note Book"<<"\t      5"<<"         50.0" << endl;
+    cout <<"   Eraser"<<"\t      20"<<"        5.75" << endl;
+    return 0;
+
+}
