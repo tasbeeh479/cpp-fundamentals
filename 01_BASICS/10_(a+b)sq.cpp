8410b9b88d2adf4027e081ae406e59c11dffd2b3
tasbeeh479 <tasbeehullah2007@gmail.com>
2026-04-17 19:28:02 +0500

    Formula

---
+++ 01_BASICS/10_(a+b)sq.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=5, b=7, result;
+    
+    result=((a*a)+(b*b)+(2*a*b))/(a+b);
+    cout<<"RESULT = "<<result;
+
+    return 0;
+}
